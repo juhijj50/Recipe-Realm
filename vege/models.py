c12aa4ea64cf9df589204f7aa1d15352094b12af
juhijj50 <juhijani38@gmail.com>
2024-10-30 14:57:22 +0530

    Updated

--- vege/models.py
+++ vege/models.py
@@ -7,6 +7,7 @@ class Receipe(models.Model):
     receipe_name = models.CharField(max_length=100)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to="receipe")
+    receipe_view_count = models.IntegerField(default=1)
     
     def __str__(self):
-        return self.receipe_name
+       return self.receipe_name
